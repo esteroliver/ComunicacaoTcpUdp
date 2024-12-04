8f45f249a171a75386076c1273fa36d27cc5f1ec
esteroliver <oliver.ester.2004@gmail.com>
2024-12-04 16:15:03 -0300

    ajeitado

--- clienteUDP.py
+++ clienteUDP.py
@@ -1,34 +1,35 @@
-import socket # biblioteca do python que possibilita criar conexões de baixo nível
-from datetime import datetime # para a funcionalidade de datetime.now()
+import socket
+from datetime import datetime 
 
-def enviar_mensagem(sock, nome, ip_servidor, porta):
+def enviar_mensagem(sock, nome, ip, porta):
     mensagem = input(f"Digite sua mensagem: ")
-    mensagem = f"[{datetime.now().strftime('%d-%m-%Y %H:%M:%S')}] {nome}: {mensagem}" # adiciona hora e
-    sock.sendto(mensagem.encode(encoding="utf-8"), (ip_servidor, porta))
+    mensagem = f"[{datetime.now().strftime('%d-%m-%Y %H:%M:%S')}] {nome}: {mensagem}"
+    sock.sendto(mensagem.encode(encoding="utf-8"), (ip, porta))
 
 def main():
+    nome = input("Olá, diga-nos seu nome: ")
+    ip = input("Digite o IP do servidor que deseja se conectar: ")
+    porta = int(input("Digite a porta do servidor que deseja se conectar: "))
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # cria socket
 
-    nome = input("Olá, diga-nos seu nome: ") # pergunta e registra nome
+    print(f"{nome} se conectou!") 
 
-    ip_servidor = input("Digite o IP do servidor que deseja se conectar: ") # pergunta e registra ip
+    while True: 
+        try: 
+            # sock.settimeout(3) # tempo para esperar por mensagens. se não mandar mais mensagens, o código vai executar outras partes
+            # data, endereco = sock.recvfrom(1024) # espera receber mensagem
+            # print(data.decode()) # mensagem decodificada e exibida na tela
+            
+            enviar_mensagem(sock, nome, ip, porta)
 
-    porta = int(input("Digite a porta do servidor que deseja se conectar: ")) # pergunta e registra a porta
-
-    sock.bind((ip_servidor, porta)) # liga o socket a endereço e porta. a partir daqui ele sempre estará ouvindo pela porta configurada
-
-    print(f"{nome} se conectou!") # printa que o nome da pessoa que se conectou
-
-    while True: # looping de envio e recebimento de mensagens
-
-        try: # tente
-            sock.settimeout(10) # tempo para esperar por mensagens. se não mandar mais mensagens, o código vai executar outras partes
-            data, endereco = sock.recvfrom(65535) # espera receber mensagem
-            print(data.decode()) # mensagem decodificada e exibida na tela
-            enviar_mensagem(sock, nome, ip_servidor, porta)
-        except socket.timeout: # exceção
-            enviar_mensagem(sock, nome, ip_servidor, porta)
+            data, endereco = sock.recvfrom(1024)
+            print(f"Resposta do servidor: {data.decode()}")
+        except socket.timeout:
+            print("Tempo de espera excedido, tentando novamente.")
+        except Exception as e:
+            print(f"Ocorreu um erro: {e}")
+            break
 
 if __name__ == "__main__":
     main()
--- servidorUDP.py
+++ servidorUDP.py
@@ -1,16 +1,24 @@
-import socket # biblioteca do python que possibilita criar conexões de baixo nível
-from datetime import datetime # para a funcionalidade de datetime.now()
-
+import socket
+from datetime import datetime
 
 def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    porta = int(input("Dgite a porta em que deseja ouvir: "))
-    sock.bind(('', porta))
-    print(f"Ouvindo na porta {porta}")
-    while True:
-        dados, endereco = sock.recvfrom(65535)
-        mensagem = f"Recebido de {str(endereco)}: {dados.decode()}"
-        sock.sendto(mensagem, (endereco, porta))
+    ip = ''
+    porta = int(input("Digite a porta em que deseja ouvir: "))
+    sock.bind((ip, porta))
 
+    print(f"Ouvindo na porta {porta}")
+
+    while True:
+        try:
+            dados, endereco = sock.recvfrom(1024)
+            mensagem = f"Recebido de {str(endereco)}: {dados.decode()}"
+            print(mensagem)
+            resposta = f"Mensagem recebida."
+            sock.sendto(resposta.encode(), endereco)
+        except Exception as e:
+            print(f"Erro: {e}")
+            break
+    sock.close()
 if __name__ == "__main__":
     main()
