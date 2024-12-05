893f2905fd9d2502dc7f2f3e4281aeedfcdd78f4
Débora Samara dos Santos Rodrigues <20231014040003@ifrn.local>
2024-12-05 07:48:41 -0300

    fix: código para escolha de protocolo funcional

--- escolhaChat.py
+++ escolhaChat.py
@@ -1,29 +1,18 @@
 import os
 
-def main(){
-    protocolo = int(input("Olá, usuário! Qual protocolo você gostaria de usar para rodar o chat?\n Digite 1 para UDP e 2 para TCP"))
+def main():
+     protocolo = int(input("Olá, usuário! Qual protocolo você gostaria de usar para rodar o chat?\n Digite 1 para UDP e 2 para TCP: "))
+     quem = int(input("Digite 1 para cliente e 2 para servidor: "))
+     if protocolo == 1:
+          if quem == 1:
+               os.system("python UDP/clienteUDP.py")
+          elif quem == 2:
+               os.system("python UDP/servidorUDP.py")
+     elif protocolo == 2:
+          if quem == 1:
+               os.system("python TCP/clienteTCP.py")
+          elif quem == 2:
+               os.system("python TCP/servidorTCP.py")
 
-    if protocolo == 1{
-        quem = int(input("Digite 1 para cliente e 2 para servidor"))
-        if quem == 1{
-             os.system("python UDP/clienteUDP.py")
-        }else if quem == 2{
-             os.system("python UDP/teTCPdorUDP")
-
-        }
-
-    }else if protocolo == 2{
- 
-        if quem == 1{
-             os.system("python TCP/clienteTCP.py")
-        }else if quem == 2{
-             os.system("python TCP/servidorTCP.py")
-
-        }
-
-    }
-}
-
-
-
-
+if __name__ == "__main__":
+     main()
